14b1554c71a84921157ce7fc414c0d241838a382
Maither <58180940+Maither@users.noreply.github.com>
2023-03-02 09:25:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.1.0-1-Linux-aarch64.sh
+sudo /bin/bash Miniconda3-py310_23.1.0-1-Linux-aarch64.sh
+sudo nano /home/pi/.bashrc
+sudo reboot -h now
